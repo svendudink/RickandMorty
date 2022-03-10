2127539af7e6e221bd7c27132a1bf8eb47b8cfa5
svendudink <78477119+svendudink@users.noreply.github.com>
2022-03-10 15:19:03 +0100

    Working with API Data

--- src/Elements/FlipImage.js
+++ src/Elements/FlipImage.js
@@ -656,17 +656,20 @@ const rickObj = {
 };
 
 const FlipImage = (props) => {
-  console.log(props.image);
   return (
-    <div key={props.itemNr} className="flip-card">
-      <div className="flip-card-inner">
-        <div className="flip-card-front">
-          <img src={props.image} alt={"Nope"} />
-        </div>
-        <div className="flip-card-back">
-          <h1>Name: {props.name}</h1>
-          <p>status: {props.status}</p>
-          <p>Species:{props.species}</p>
+    <div>
+      <div className="grid-item">
+        <div className="flip-card">
+          <div className="flip-card-inner">
+            <div className="flip-card-front">
+              <img src={props.image} alt={"Nope"} />
+            </div>
+            <div className="flip-card-back">
+              <h1>Name: {props.name}</h1>
+              <p>status: {props.status}</p>
+              <p>Species:{props.species}</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
